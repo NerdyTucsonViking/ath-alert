24d9efa183fd1cb04110d82aa09bfb3b4ad8fbf3
NerdyTucsonViking <143241121+NerdyTucsonViking@users.noreply.github.com>
2026-03-31 18:59:07 -0700

    Update Compose compiler version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,11 @@ android {
         compose = true
         buildConfig = true
     }
+
+composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.14"
+}
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
